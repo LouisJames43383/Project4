d16d1157f272ec207fe83e096d757c797e56c1cc
Noah Bond <44420894+Noah-Bond@users.noreply.github.com>
2020-11-02 23:08:32 -0500

    Added tokens

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
+  consumer_key:         'AeGidairR1G7hCp9bMktrBkvE',
+  consumer_secret:      'GbQ8NYyL5fH2JP0KRfI3rMICaxR2z4z2HV7fb2rWvyKWMoZ3Po',
+  access_token:         'AAAAAAAAAAAAAAAAAAAAAMWCJQEAAAAA6T%2FY6WdoDwe%2BwgeYChnLD8usbJk%3DN9p0xJfn2lOsWKGBHZricnXr6LGdYlBkkM3Y9puvEUn5RZPHwi',
   access_token_secret:  'blah'
 }

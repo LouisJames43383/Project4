f619cf93f146cdcc94c21e61e953ebb5c21a45e2
LouisJames43383 <70409496+LouisJames43383@users.noreply.github.com>
2020-11-03 14:31:34 -0500

    reverse1.0(posting the reversed)

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var tenetSearch = {q: "#tenet", count: 10, result_type: "recent"};
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
+	T.get('search/tweets', tenetSearch, function (error, data) {
 	  // log out any errors and responses
 	  console.log(error, data);
 	  // If our search request to the server had no errors...
@@ -18,9 +18,22 @@ function retweetLatest() {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
 		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+        var tweetContent = ''
+        tweetContent = tweetContent.concat(data.statuses[0].text);
+        var reverse = ''
+        for (var i=tweetContent.length-1; i>=0; i--) {
+            reverse = reverse.concat(tweetContent[i]);
+        }
+        var rP = {
+            status: reverse
+        }
+        var a = T.post('statuses/update',rP)
+		T.post('statuses/retweet/' + retweetId, a, function (error, response) {
 			if (response) {
 				console.log('Success! Check your bot, it should have retweeted something.')
+                console.log(reverse)
+                tweetContent = ''
+                reverse = ''
 			}
 			// If there was an error with our Twitter call, we print it out here.
 			if (error) {

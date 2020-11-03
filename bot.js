23c7a7436fc76c65a88da0515746e91890b430f0
Noah Bond <44420894+Noah-Bond@users.noreply.github.com>
2020-11-03 12:39:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var T = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
 var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+var tenetSearch = {q: "#tenet", count: 10, result_type: "recent"}; 
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {

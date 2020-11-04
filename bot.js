71cbd6504789ed4f2072ebdcabc8cabd140be645
LouisJames43383 <70409496+LouisJames43383@users.noreply.github.com>
2020-11-04 17:02:03 -0500

    FinalFinalEdition

--- bot.js
+++ bot.js
@@ -8,7 +8,6 @@ var T = new Twit(require('./config.js'));
 var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
 var tenetSearch = {q: "#tenet", count: 10, result_type: "recent"}; 
 var thwgSearch = {q: "#thwg", count: 10, result_type: "recent"};
-var tweetbotSearch = {q: "#tweetbot", count: 10, result_type: "recent"};
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
@@ -75,27 +74,18 @@ function thwgLatest() {
 }
 
 // a method that likes tweets with hashtag tweetbot
-function tweetbotLike() {
-	T.get('search/tweets', tweetbotSearch, function(error, data) {
-		console.log(error, data);
-	    var likeId = data.statuses[0].id_str;
-	    T.post('favorites/create', {id:likeId}, function(error ,response) {
-		    if (response) {
-			console.log("just liked a post")
-		    }
-		    if (error) {
-			console.log('There was an error with Twitter: ', error);
-			}
-	    });
-	    console.log(data);
-	});
-}
+T.get('search/tweets', { q: '#tweetbot', count: 10}, function(err, data, response) {
+    var likeId = data.statuses[0].id_str;
+    T.post('favorites/create', {id:likeId}, function(err,data,response) {
+        console.log("just liked a post")
+    });
+    console.log(data);
+});
+
 // Try to retweet something as soon as we run the program...
 retweetLatest();
 thwgLatest();
-tweetbotLike();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60 * 24); //daily
 setInterval(thwgLatest, 1000 * 60 * 60 * 24 * 7); //weekly
-setInterval(tweetbotLike(), 1000 * 60 * 60); //hourly
